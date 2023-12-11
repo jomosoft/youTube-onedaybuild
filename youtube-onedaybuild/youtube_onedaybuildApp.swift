ef1be3283672cf168e15946d5e90898fc97f29dd
John Frings <johan@jomosoft.nl>
2023-12-11 13:52:59 +0100

    initial xcode project creation

---
+++ youtube-onedaybuild/youtube_onedaybuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_onedaybuildApp.swift
+//  youtube-onedaybuild
+//
+//  Created by J.H.H. Frings on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct youtube_onedaybuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
